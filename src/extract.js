6144f6f2284d699029d2260994791abd1dcd56ee
Whitebread <williampanebianco@gmail.com>
2019-10-12 19:39:03 -0400

    Initial commit

---
+++ src/extract.js
@@ -0,0 +1,90 @@
+/**
+ * Extract Starcraft II quotes from online
+ */
+'use strict';
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const URL = 'https://starcraft.fandom.com/wiki/StarCraft_II_unit_quotations';
+const OUTPUT_DIRECTORY = './quotes/extract';
+
+/**
+ * Extract all quotes from specified uri for faction
+ * @param {string} faction - the faction to extract quotes from
+ */
+async function quotesExtractor(faction) {
+  console.log('SCV READY');
+  console.log(`EXTRACTING INFORMATION FOR: ${faction}`);
+
+  let unit, action;
+  let actions = [];
+  let quotes = [];
+  let quote = {};
+
+  try {
+    const response = await axios.get(URL);
+
+    let $ = cheerio.load(response.data);
+    let current_element = $(`#${faction}`).parent();
+
+    do {
+      current_element = $(current_element).next();
+
+      if (current_element.is('h4')) {
+        unit = current_element.children().attr('id');
+      }
+
+      if (current_element.is('table')) {
+        current_element
+          .children()
+          .children()
+          .each((i, row) => {
+            if (i % 2 === 0) {
+              let columns = $(row).children();
+
+              columns.each((i, column) => {
+                actions.push($(column).text());
+              });
+            } else {
+              let columns = $(row).children();
+
+              columns.each((i, column) => {
+                action = actions.pop();
+                $('li', column).each((i, value) => {
+                  quote = {
+                    value: $(value).text(),
+                    faction: faction.charAt(0).toUpperCase() + faction.slice(1),
+                    unit: unit,
+                    action: action
+                  };
+                  quotes.push(quote);
+                });
+              });
+            }
+          });
+      }
+    } while (
+      $(current_element).next()[0] !== undefined &&
+      !$(current_element)
+        .next()
+        .is('h2')
+    );
+  } catch (err) {
+    console.log(err);
+  }
+
+  let data = JSON.stringify(quotes, null, 2);
+
+  fs.mkdir(OUTPUT_DIRECTORY, { recursive: true }, err => {
+    if (err) throw err;
+  });
+
+  fs.writeFileSync(`${OUTPUT_DIRECTORY}/${faction.toLowerCase()}.json`, data);
+  console.log('JOBS FINISHED');
+  console.log(`OUTPUT: ${OUTPUT_DIRECTORY}/${faction.toLowerCase()}.json`);
+}
+
+quotesExtractor('Terran');
+quotesExtractor('Zerg');
+quotesExtractor('Protoss');
---
+++ src/transform.js
@@ -0,0 +1,170 @@
+/**
+ * Transform Starcraft II quotes
+ */
+'use strict';
+const fs = require('fs');
+const uuidv4 = require('uuid/v4');
+
+const INPUT_DIRECTORY = './quotes/extract';
+const OUTPUT_DIRECTORY = './quotes/transform';
+
+/**
+ * Ensure strings are clean / neat for .json file
+ * @param {string} value - string to make 'clean'
+ */
+function cleanString(value) {
+  return (
+    value
+      // Remove newlines / carriage returns
+      .replace(/\r?\n|\r/g, ' ')
+      // Remove quotations
+      .replace(/["]+/g, '')
+      // Remove ( )
+      .replace(/\([^)]*\)/g, '')
+      // Remove *
+      .replace(/\*[^*]*\*/g, '')
+      // Remove < >
+      .replace(/\<[^*]*\>/g, '')
+      // Remove double spaces
+      .replace(/  /g, ' ')
+      // Add space after question mark
+      .replace(/(\?)([A-Za-z])/g, '$1 $2')
+      .trim()
+  );
+}
+
+/**
+ * Ensure unit name is clean / correct
+ * @param {string} unit - unit to 'clean'
+ */
+function cleanQuoteUnit(unit) {
+  if (unit.includes(`Fenix`)) {
+    return 'Talandar';
+  }
+
+  return cleanString(unit)
+    .replace(/\.2F/g, ' ')
+    .replace(/_/g, ' ');
+}
+
+/**
+ * Ensure action is clean / correct
+ * @param {string} action - action to 'clean'
+ */
+function cleanQuoteAction(action) {
+  if (action.includes('Repeatedly selected')) {
+    return 'Pissed';
+  } else if (action.includes('Other lines')) {
+    return 'Other';
+  } else if (action.includes('Attack order')) {
+    return 'Attack';
+  } else if (action.includes('Move order')) {
+    return 'Move';
+  } else if (action.includes('When attacked')) {
+    return 'Attacked';
+  }
+
+  return cleanString(action);
+}
+
+/**
+ * Ensure faction is clean / correct
+ * @param {string} faction - faction to 'clean'
+ */
+function cleanQuoteFaction(faction) {
+  return cleanString(faction);
+}
+
+/**
+ * Ensure the actual quote is clean / correct
+ * @param {string} value - the actual quote to 'clean'
+ */
+function cleanQuoteValue(value) {
+  return cleanString(value);
+}
+
+/**
+ * Transforms all quotes including metadata
+ * @param {string} input - the name of the JSON file to transform
+ * @param {string} output  - the location of the transformed JSON file
+ */
+function quoteTransformer(input, output) {
+  console.log('WHO CALLED IN THE FLEET');
+  console.log(`TRANSFORMING: ${input}`);
+
+  let cleanQuotes = [];
+
+  let rawData = fs.readFileSync(input);
+  let quotes = JSON.parse(rawData);
+
+  quotes.forEach(function(quote) {
+    let cleanQuote = {
+      value: cleanQuoteValue(quote['value']),
+      faction: cleanQuoteFaction(quote['faction']),
+      unit: cleanQuoteUnit(quote['unit']),
+      action: cleanQuoteAction(quote['action'])
+    };
+
+    if (cleanQuote['value'] !== '') {
+      cleanQuotes.push(cleanQuote);
+    }
+  });
+
+  let data = JSON.stringify(cleanQuotes, null, 2);
+
+  fs.writeFileSync(output, data);
+  console.log('WEAPONS CHARGED AND READY');
+  console.log(`OUTPUT: ${output}`);
+
+  return cleanQuotes;
+}
+
+/**
+ * Adds short uuid for all quote objects
+ * @param {array} quotes - an array of quote objects
+ */
+function shortId(quotes) {
+  let uniqueIds = [];
+  quotes.forEach(function(quote, i) {
+    let uniqueId;
+    let isUnique;
+    do {
+      uniqueId = uuidv4().split('-')[0];
+      isUnique = false;
+      if (!uniqueIds.includes(uniqueId)) {
+        uniqueIds.push(uniqueId);
+        isUnique = true;
+      }
+    } while (!isUnique);
+
+    quote['id'] = uniqueId;
+  });
+}
+
+fs.mkdir(OUTPUT_DIRECTORY, { recursive: true }, err => {
+  if (err) throw err;
+});
+
+// succubus.json was manually created. Not found on wowwiki
+const FILES = ['protoss.json', 'terran.json', 'zerg.json'];
+
+let quotes = [];
+
+FILES.forEach(function(file) {
+  quotes = quotes.concat(
+    quoteTransformer(
+      `${INPUT_DIRECTORY}/${file}`,
+      `${OUTPUT_DIRECTORY}/${file}`
+    )
+  );
+});
+
+shortId(quotes);
+
+let data = JSON.stringify(quotes, null, 2);
+
+if (!fs.existsSync('./quotes/starcraft-2-quotes.json')) {
+  fs.writeFileSync('./quotes/starcraft-2-quotes.json', data);
+  console.log('WEAPONS CHARGED AND READY');
+  console.log(`OUTPUT: ./quotes/starcraft-2-quotes.json`);
+}
